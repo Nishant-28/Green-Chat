396768c61330617b65a73b93f9c9a25c9da32190
Yash Khare <yash2010118@akgec.ac.in>
2024-01-11 10:46:56 +0530

    folder setup

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { cn } from "@/lib/utils";
+
+const state=false // can be used as dynamic cases
+
+export default function Home() {
+  return (
+    <div className="">
+      This is a protected route
+    </div>
+  )
+}
