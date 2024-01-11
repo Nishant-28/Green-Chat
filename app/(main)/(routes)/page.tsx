b6868ecde34df84d0e1d916c5553302eb432cce0
Yash Khare <yash2010118@akgec.ac.in>
2024-01-11 11:04:13 +0530

    setup authentication using clerk

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,11 +1,14 @@
 import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
 
 const state=false // can be used as dynamic cases
 
 export default function Home() {
   return (
     <div className="">
-      This is a protected route
+      <UserButton
+        afterSignOutUrl="/"
+      />
     </div>
   )
 }
