4340aeb8909ce6314a7a509362cc30d7853a2e1c
Yash Khare <yash2010118@akgec.ac.in>
2024-01-12 22:20:54 +0530

    setup sidebar

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -23,5 +23,5 @@ export const NavigationAction = () => {
                 </button>
             </ActionToolTip>
         </div>
-     );
+    );
 }
--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,16 +1,22 @@
 import { redirect } from "next/navigation";
 
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { ModeToggle } from "@/components/mode-toggle";
+
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
 import { NavigationAction } from "./navigation-action";
+import { NavigationItem } from "./navigation-item";
+import { UserButton } from "@clerk/nextjs";
 
 export const NavigationSidebar = async () => {
 
     const profile = await currentProfile();
     if(!profile) return redirect("/")
 
-    const serves = await db.server.findMany({
+    const servers = await db.server.findMany({
         where:{
             members:{
                 some:{
@@ -25,6 +31,31 @@ export const NavigationSidebar = async () => {
             dark:bg-[#1E1F22] py-3"
         >
             <NavigationAction/>
+            <Separator
+                className="h-[2px] bg-zinc-100 dark:bg-zinc-700 rounded-md w-10 mx-auto"
+            />
+            <ScrollArea className="flex-1 w-full">
+                {servers.map((server)=>(
+                    <div key = {server.id}>
+                        <NavigationItem 
+                            id={server.id}
+                            name={server.name}
+                            imageUrl={server.imageUrl}
+                        />
+                    </div>
+                ))}
+            </ScrollArea>
+            <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+                <ModeToggle/>
+                <UserButton
+                    afterSignOutUrl="/"
+                    appearance={{
+                        elements:{
+                            avatarBox:"h-[48px] w-[48px]"
+                        }
+                    }}
+                />
+            </div>
         </div>
      );
 }
