ed705671faffa856ad53edcd365ccd6a07a0e48d
Yash Khare <yash2010118@akgec.ac.in>
2024-01-14 21:04:48 +0530

    server: update inviteCode and schema for invitation

---
+++ app/(invite)/(routes)/invite/[invite_code]/page.tsx
@@ -0,0 +1,63 @@
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
+import { currentProfile } from "@/lib/current-profile";
+
+interface InviteCodePageProps {
+  params: {
+    invite_code: string;
+  };
+};
+
+const InviteCodePage = async ({
+  params
+}: InviteCodePageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  if (!params.invite_code) {
+    return redirect("/");
+  }
+
+  const existingServer = await db.server.findFirst({
+    where: {
+      inviteCode: params.invite_code,
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    }
+  });
+
+  if (existingServer) {
+    return redirect(`/servers/${existingServer.id}`);
+  }
+
+  const server = await db.server.update({
+    where: {
+      inviteCode: params.invite_code,
+    },
+    data: {
+      members: {
+        create: [
+          {
+            profileId: profile.id,
+          }
+        ]
+      }
+    }
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+  
+  return null;
+}
+ 
+export default InviteCodePage;
